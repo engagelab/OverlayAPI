43778e28f47b9c9847b1af62cdc681f0977bf345
Fahied <fahied@gmail.com>
2012-12-13 18:11:28 +0100

    matched images tree to instagramm

--- app/data/Images.java
+++ app/data/Images.java
@@ -1,23 +1,29 @@
 package data;
 
+import external.Constants;
+
 public class Images {
 	
-	public String low_resolution;
-	public String thumbnail;
-	public String standard_resolution;
+	public Image low_resolution;
+	public Image thumbnail;
+	public Image standard_resolution;
 	
 	public Images() {
 		
 	}
 	
 	public Images(String standard_resolution) {
-		this.standard_resolution = standard_resolution;
+		
+		String url = Constants.SERVER_NAME+"/image/"+standard_resolution;
+		this.standard_resolution = new Image(url, 612, 612);
+		this.low_resolution = new Image();
+		this.thumbnail = new Image();
 		
 	}
 	
 	
 	
-	public Images(String low_resolution, String thumbnail, String standard_resolution) 
+	public Images(Image low_resolution, Image thumbnail, Image standard_resolution) 
 	{
 		this.low_resolution = low_resolution;
 		this.thumbnail = thumbnail;
