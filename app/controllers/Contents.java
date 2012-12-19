09dfd4a0e36baa6ca0fb3be265803702d895a4f1
Fahied <fahied@gmail.com>
2012-12-19 15:25:53 +0100

    fixed description view html

--- app/controllers/Contents.java
+++ app/controllers/Contents.java
@@ -1,12 +1,16 @@
 package controllers;
 
+import org.codehaus.jackson.JsonNode;
+
 import external.InstagramParser;
 import helpers.TwitterHelper;
 import models.Feature;
+import models.User;
 import play.api.templates.Html;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import static play.libs.Json.toJson;
 
 public class Contents extends Controller{
 	
@@ -16,35 +20,42 @@ public class Contents extends Controller{
 	
 	
 	
-	
-	public static Result contentOfFeature(String id)
-	{
+	public static Result contentOfFeature(String id) {
 		Feature feature = Feature.find().byId(id);
-		
+
 		if (feature == null) {
 			return ok("This POI does not exist anymore.");
-		}
-		else {
+		} else {
+
 			String decString = feature.properties.get("description").toString();
-			
 
 			decString = decString.replaceAll("^\"|\"$", "");
 			String description = TwitterHelper.parse(decString, "Overlay");
 			String image = "";
-			
+			User user = new User();
+
 			if (feature.properties.get("standard_resolution") != null) {
-				image  = "<div id=\"image-holder\"> " +
-	                    "<img src="+feature.properties.get("standard_resolution").toString()+" alt=\"Smiley face\"  width=\"612\" > " +
-	                    "</div> " ;
+				image = "<div id=\"image-holder\"> "
+						+ "<img src="
+						+ feature.properties.get("standard_resolution")
+								.toString()
+						+ " alt=\"Smiley face\"  width=\"612\" > " + "</div> ";
 			}
 			
-			Html content = new Html(image+description);
-			
-			
-			return ok(index.render(feature,content));
-			
+			if (feature.properties.get("user") != null) {
+				
+				JsonNode userNode = toJson(feature.properties.get("user"));
+				user.id =userNode.get("id").asText();
+				user.full_name = userNode.get("full_name").asText();;
+				
+			}
+
+			Html content = new Html(image + description);
+
+			return ok(index.render(user, content));
+
 		}
-		
+
 	}
 	
 	
