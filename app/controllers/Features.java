0b2ef67ca3ea82238551ec47a34f2c6ff54c1307
Fahied <fahied@gmail.com>
2013-02-12 12:05:43 +0100

    fixed description text in Udate Feature

--- app/controllers/Features.java
+++ app/controllers/Features.java
@@ -220,6 +220,7 @@ public class Features extends Controller {
 		if (source_type.equalsIgnoreCase("overlay")) 
 		{
 			String description = (String) propertiesJSON.get("description");
+			properties.put("description", description);
 			// Formulate the label of the POI, using first sentence
 			// it is named as "name" as a convention of KML standard
 			String name = createCaptionFromDescription(description);
